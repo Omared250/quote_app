da8525ebdbc2642c554b7b0137d3a75f2aaa8724
Omar <omared250@gmail.com>
2022-08-10 17:07:42 +0200

    quetting a new quote with a click

--- src/js/controller.js
+++ src/js/controller.js
@@ -5,20 +5,24 @@ const loadAdvice = async function() {
     try {
         await model.loadQuote();
         quoteView.render(model.quote);
+        getQuote();
     } catch (err) {
         console.error(err);
     }
 }
 
-
-const getQuote = function() {
+const getQuote = async function() {
     try {
-        
+        const dice = document.querySelector('.dice');
+        dice.addEventListener('click', async function(e) {
+            e.preventDefault();
+            quoteView.clearContainer();
+            await loadAdvice();
+        })
     } catch (err) {
         console.error(err);
     }
 }
-
 
 const init = function() {
     quoteView.addHandlerHome(loadAdvice)
--- src/js/quoteView.js
+++ src/js/quoteView.js
@@ -12,13 +12,16 @@ class QuoteView {
         this._parentEl.insertAdjacentHTML('beforeend', markup);
     }
 
+    clearContainer() {
+        this._parentEl.innerHTML = '';
+    }
+
     _generateQuoteMarkup() {
         return `
         <h3>Advice #${this._data.id}</h3>
-        <h2>"${this._data.advice}"
-        </h2>
-        <img src="./src/images/pattern-divider-desktop.svg"><br>
-        <div class="dice"><img src="./src/images/icon-dice.svg">
+        <h2>"${this._data.advice}"</h2>
+        <img src="./src/images/pattern-divider-desktop.svg" alt="divider"><br>
+        <div class="dice"><img src="./src/images/icon-dice.svg" alt="dice">
         </div>`
     }
 
